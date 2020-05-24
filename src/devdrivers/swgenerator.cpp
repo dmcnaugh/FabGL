f18638291b84782a2f13865f02e2f3995f969b71
dave <dave@mackeneight.net>
2020-05-24 18:28:56 +1000

    Upgrade: for edp-idf 4.0

--- src/devdrivers/swgenerator.cpp
+++ src/devdrivers/swgenerator.cpp
@@ -29,7 +29,7 @@
 #include "soc/i2s_struct.h"
 #include "soc/i2s_reg.h"
 #include "driver/periph_ctrl.h"
-#include "rom/lldesc.h"
+#include "esp32/rom/lldesc.h"
 #include "soc/rtc.h"
 
 #include "fabutils.h"
