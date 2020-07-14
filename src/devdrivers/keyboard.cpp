4d36a87abf4ff3cc0606026512a7c9d8870f5860
dave <dave@mackeneight.net>
2020-07-14 10:12:58 +1000

    Update: pin CCT, KRT and SC2VK taks to CPU1

--- src/devdrivers/keyboard.cpp
+++ src/devdrivers/keyboard.cpp
@@ -494,7 +494,7 @@ void Keyboard::begin(bool generateVirtualKeys, bool createVKQueue, int PS2Port)
   if (generateVirtualKeys || createVKQueue) {
     if (createVKQueue)
       m_virtualKeyQueue = xQueueCreate(FABGLIB_KEYBOARD_VIRTUALKEY_QUEUE_SIZE, sizeof(uint16_t));
-    xTaskCreate(&SCodeToVKConverterTask, "SC2VK", FABGLIB_SCODETOVK_TASK_STACK_SIZE, this, FABGLIB_SCODETOVK_TASK_PRIORITY, &m_SCodeToVKConverterTask);
+    xTaskCreatePinnedToCore(&SCodeToVKConverterTask, "SC2VK", FABGLIB_SCODETOVK_TASK_STACK_SIZE, this, FABGLIB_SCODETOVK_TASK_PRIORITY, &m_SCodeToVKConverterTask, 1);
   }
 }
 
